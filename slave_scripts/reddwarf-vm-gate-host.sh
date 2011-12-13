8e7f3026c0aedc6a8c8d5a37f9c53baf979e2ae6
hub-cap <mbasnight@gmail.com>
2011-12-13 13:11:30 -0600

    Adding the hostname to the gate script

--- slave_scripts/reddwarf-vm-gate-host.sh
+++ slave_scripts/reddwarf-vm-gate-host.sh
@@ -53,6 +53,7 @@ ifconfig eth1:0 33.33.33.11 netmask 255.255.255.255
 ifconfig eth1:1 10.0.4.15 netmask 255.255.255.255
 
 echo '10.0.4.15	apt.rackspace.com' >> /etc/hosts
+hostname host
 
 # Chown manually since the whoami isint correct above yet
 chown -R vagrant /opt/reddwarf/
